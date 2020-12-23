3e44c231b502f60b8ec0478a48e99f158ac60e9e
Leonardo <leonardo.luize2@gmail.com>
2020-12-23 14:39:20 -0300

    add resize function

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const sharp = require('sharp');
+
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 
 function resize(path, width){
     
+    sharp(path).resize({width: width}).toFile('./temp/output_resize.jpg',(error) => {
+        if(error){
+            console.log(error);
+        }else{
+            console.log("imagem redimensionada com sucesso!!! :D")
+        }
+    })
+
 }
+
+resize(path, width);
